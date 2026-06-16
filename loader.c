4036b400a3ba92dc45189e7e609f83df03e2336a
fabrice1618 <fabrice1618@gmail.com>
2026-06-16 19:11:10 +0200

    commit initial

---
+++ loader.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <mysql/mysql.h>
+
+int main()
+{
+    int tcp_sock, c;
+    socklen_t l;
+    struct sockaddr_in a, b;
+    char buf[1024];
+    float *f;
+    MYSQL *m;
+    char q[512];
+
+    tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    a.sin_family = AF_INET;
+    a.sin_port = htons(1618);
+    a.sin_addr.s_addr = INADDR_ANY;
+
+    bind(tcp_sock, (struct sockaddr*)&a, sizeof(a));
+
+    listen(tcp_sock, 5);
+
+    m = mysql_init(NULL);
+    mysql_real_connect(m, "localhost", "meteo", "abcd", "meteo", 0, NULL, 0);
+
+    while(1)
+    {
+        l = sizeof(b);
+        c = accept(tcp_sock, (struct sockaddr*)&b, &l);
+
+        read(c, buf, 1024);
+
+        f = (float*)buf;
+
+        float t = f[0];
+        float h = f[1];
+        float p = f[2];
+
+        sprintf(q, "INSERT INTO data VALUES(%f,%f,%f)", t, h, p);
+
+        mysql_query(m, q);
+
+        printf("t=%f h=%f p=%f\n", t, h, p);
+
+        close(c);
+    }
+
+    mysql_close(m);
+
+    return 0;
+}
